17f3544feeba3d59c48537872401c7278424f8b4
SARTHAKTYAGIjii <sarthaktyagi176@gmail.com>
2024-05-01 12:01:30 +0530

    i have commit the HTML,CSS,JS file

---
+++ JSCODE.js
@@ -0,0 +1,193 @@
+import { LightningElement, track} from "lwc";
+import createCase from "@salesforce/apex/CaseWithDetails.createCase";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import createCase1 from "@salesforce/apex/CaseWithDetails.createCase1";
+
+export default class CaseWithDetails extends LightningElement {
+    @track selectedCountry;
+    customerName;
+    cityName;
+    PhoneNum;
+    street;
+    caseData;
+    PostCode;
+    @track textAreaContent;
+    @track emailVal;
+    @track numberOfProduct;
+    @track ReasonForReturn;
+    @track ProductName;
+    @track selectedRequestType;
+    files;
+    @track isButtonDisabled = true;
+    showImageName;
+    countryOptions = [
+        { label: "United Kingdom", value: "UK" },
+        { label: "India", value: "India" },
+        { label: "United States of America", value: "USA" }
+    ];
+    Products = [{ label: "1", value: "1" }];
+    ReasonReturn = [
+        { label: "Faulty/Defective Product", value: "Faulty" },
+        { label: "Product Damaged", value: "ProductDamaged" },
+        { label: "Incorrect Item Sent", value: "IncorrectItemSent" },
+        { label: "Change of mind", value: "Changeofmind" }
+    ];
+    ProductOption = [];
+    requestTypeOptions = [];
+
+    async handleemail(event) {
+        this.emailVal = event.target.value;
+        console.log(this.emailVal);
+        if (this.isEmailValid(this.emailVal)) {
+            await createCase({ Eml: this.emailVal })
+                .then((result) => {
+                    if (result.length > 0) {
+                        console.log(result);
+                        this.ProductOption = result.map((ele) => ({
+                            label: ele.Name,
+                            value: ele.Id
+                        }));
+                    }
+                })
+                .catch(() => {
+                    this.dispatchEvent(
+                        new ShowToastEvent({
+                            title: "Failed",
+                            message: "No data found write the correct email",
+                            variant: "error"
+                        })
+                    );
+                });
+        }
+    }
+
+    isEmailValid(email) {
+        const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/;
+        return emailRegex.test(email);
+    }
+    handleAllOnClickFunction(event) {
+        const fieldName = event.target.name;
+        const value = event.target.value || event.detail.value;
+
+        switch (fieldName) {
+            case "PhoneNumber":
+                this.PhoneNum = value;
+                break;
+            case "customerName":
+                this.customerName = value;
+                console.log(this.customerName);
+                break;
+            case "Postcode":
+                this.PostCode = value;
+                break;
+            case "street":
+                this.street = value;
+                break;
+            case "cityName":
+                this.cityName = value;
+                break;
+            case "selectedCountry":
+                this.selectedCountry = value;
+                break;
+            case "numberOfProduct":
+                this.numberOfProduct = value;
+                console.log("this is the numberofProduct", this.numberOfProduct);
+                break;
+            case "ProductName":
+                this.ProductName = value;
+                break;
+            default:
+                console.log("here");
+                break;
+        }
+    }
+
+    handleKeyup(event) {
+        const keyPressed = event.key;
+        if (!/[a-zA-Z]/.test(keyPressed)) {
+            const msg = this.template.querySelector(".showmsg");
+            msg.textContent = "Invalid Character,In Name Only use alphabets";
+            setTimeout(() => {
+                msg.textContent = "";
+            }, 3000);
+        }
+    }
+
+    handleReason(event) {
+        this.ReasonForReturn = event.detail.value;
+        if (
+            this.ReasonForReturn === "Faulty" ||
+            this.ReasonForReturn === "ProductDamaged"
+        ) {
+            this.selectedRequestType = "Refund";
+            this.requestTypeOptions = [{ label: "Refund", value: "Refund" }];
+        } else if (
+            this.ReasonForReturn === "IncorrectItemSent" ||
+            this.ReasonForReturn === "Changeofmind"
+        ) {
+            this.selectedRequestType = "Return";
+            this.requestTypeOptions = [{ label: "Return", value: "Return" }];
+        }
+    }
+    handleUploadFinished(event) {
+        this.files = event.detail.files[0].documentId;
+        this.showImageName = this.template.querySelector(".showImageName");
+        this.showImageName.textContent = event.detail.files[0].name;
+    }
+    deleteUploadedImage() {
+        this.showImageName = this.template.querySelector(".showImageName");
+        this.showImageName.textContent = "";
+        this.files = null;
+    }
+    handleSave() {
+        this.caseData = {
+            contentDocumentId: this.files,
+            productId: this.ProductName,
+            handlingReason: this.ReasonForReturn,
+            cntofProduct: this.numberOfProduct,
+            contactCountry: this.selectedCountry,
+            contactCity: this.cityName,
+            contactStreet: this.street,
+            contactPostalCode: this.PostCode,
+            customerName: this.customerName,
+            contactPhone: this.PhoneNum,
+            contactEmail: this.emailVal
+        };
+        console.log('here is the caseData for the js file',JSON.stringify(this.caseData));
+        if (!this.PhoneNum ||!this.PostCode || !this.ProductName || !this.customerName || !this.street || !this.files || !this.ReasonForReturn || !this.selectedCountry ||!this.cityName ||!this.emailVal ||!this.numberOfProduct || !this.selectedRequestType){
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: "failed",
+                    message: "one of the field is empty",
+                    variant: "error"
+                })
+            );
+        }
+        else{
+           createCase1({ caseData1: this.caseData });
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: "success",
+                message: "Record is Created",
+                variant: "success"
+            })
+        );
+        this.PhoneNum = "";
+        this.PostCode = "";
+        this.ProductName = "";
+        this.customerName = "";
+        this.street = "";
+        this.files = "";
+        this.ReasonForReturn = "";
+        this.selectedCountry = "";
+        this.cityName = "";
+        this.emailVal = "";
+        this.numberOfProduct = "";
+        this.selectedRequestType = "";
+        this.showImageName = this.template.querySelector(".showImageName");
+        this.showImageName.textContent = "";
+        this.files = null;
+        this.textAreaContent = "";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # CaseCreationComponet
+so this is the redme file
+<br>
+so in this repo i will post my code of CaseCreaseComponent in this folder you will have all three files of the code like HTML,CSS,Js.
+<br>
+here is the list of functionality that i have build.
+<br>
+Create a LWC component as shown in the image.
+<br>
+Create City, Country, street, Postcode, request type, and reason for return fields on the case.
+<br>
+1. Customer Name - (Account Name)
+<br>
+2. City (Customer city)
+<br>
+3. Country (Picklist field (United Kingdom, India, United States of America))
+<br>
+4. Street (Customer street)
+<br>
+5. Postcode (Customer postcode)<br>
+6. Order Email address (If it matches the Account email address then we will find related opportunity and Opportunity line items. If we get the Opportunity line items then we have to show Products in the Product input field.)<br>
+7. Customer phone number<br>
+8. Product to be returned (At a time only one product can be returned.)<br>
+9. Reason for return (Picklist (Faulty/Defective Product, Product Damaged, Incorrect Item Sent, Change of mind))<br>
+10. Product (Picklist - (Should be fetched from the Account's Opportunity's Opportunity Product))(If the Order email address matches the account's email)<br>
+10. Request type (if Reason for return (Faulty/Defective Product, Product Damaged) is selected then it should be 'Refund' and if Reason for return (Incorrect Item Sent, Change of mind) is selected then it should be 'Return')<br>
+11. Details of return or refund. (Enter description)<br>
+12. Photo Upload (for uploading photo of product)<br>
+13. Delete photo (for deleting the uploaded photo)<br>
+14. Create Case. (When clicking on the case button a case should be created in the org with all the fields populated. (Account, Opportunity, and product auto-populated))<br>
+
+All the fields should be mandatory. Without entering any field the Create case button should not be enabled.<br>
+
+Notes:
+- If the Order email address(6 point) is not found then shows an error email not found. Please enter a valid email.<br>
+- Phone numbers should be 10 numbers.<br>
+- Customer names only can be alphabets inputs.<br>
+- The component should look good (Correct Padding should be done)
+Create a community page Refund Request and deploy your component on that page.<br>
+Also make sure, guest user also be able to submit the refund request via this page.<br>
